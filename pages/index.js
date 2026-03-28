3cde3ab7be33bf717464adedaa0a4522240491d6
Lucas <lucasmariano.2002@gmail.com>
2026-03-28 01:13:38 +0000

    mudança na frase do arquivo

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>O amor da minha vida tem nome e sobrenome, tem um sorriso lindo e uma jeito que me encanta. Te amo eternamente meu amor! </h1>
+    return <h1>O amor da minha vida tem nome e sobrenome, tem um sorriso lindo e um jeito que me encanta. Te amo eternamente meu amor! </h1>
 }
 export default Home;
 
